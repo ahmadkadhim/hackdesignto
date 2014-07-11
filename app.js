2fe3ddb7e8594d455f69d59bc922378e002b9e8d
AhmadKadhim <akadhim13@gmail.com>
2014-07-11 14:01:33 -0400

    Icons

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.compress());
+
+var oneDay = 86400000;
+
+app.use(express.static(__dirname + '/public', { maxAge: oneDay }));
 
 var server = app.listen(3000, function() {
 	console.log('Listening on port %d', server.address().port);
